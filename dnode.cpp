9d6099d010acc85a6f98a366b98d4105505199e6
Anshuman <Anshuman@ucsb.edu>
2020-02-25 15:27:11 -0800

    dnode

---
+++ dnode.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "dnode.h"
+
+using namespace std;
+
+int main(){
+    
+}
---
+++ dnode.h
@@ -0,0 +1,16 @@
+template <class T>
+class dnode
+{
+private:
+    T data;
+    // forward pointer
+    dnode<T> *next;
+    // backward pointer
+    dnode<T> *last;
+
+public:
+    // TODO:add member functions
+    dnode(T d, dnode<T> *n, dnode<T> *l){
+        
+    }
+};
