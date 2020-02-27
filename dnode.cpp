34900d5cbd96d3314ef4882be23e1d594dd237fd
Anshuman-UCSB <Anshuman@ucsb.edu>
2020-02-26 16:54:56 -0800

    dnode method

--- dnode.cpp
+++ dnode.cpp
@@ -12,6 +12,26 @@ dnode<T>::dnode(T d, dnode<T> *n, dnode<T> *l){
     data = d;
 }
 
+template <class T>
+dnode<T>::dnode(T d)
+{
+    next = 0;
+    last = 0;
+    data = d;
+}
 
+template <class T>
+void dnode<T>::insert(dnode<T> *node, T val){
+    dnode<T> *newNode = new dnode<T>(val);
+    dnode<T> *after = node;
+    while(after->next != 0){
+        if(newNode)
+    }
+    
+}
+
+bool operator<(const dnode<int> &n1, const dnode<int> &n2){
+    return (n1.data<n2.data);
+}
 
 #endif
--- dnode.h
+++ dnode.h
@@ -1,14 +1,16 @@
 template <class T>
 class dnode
 {
-private:
+public:
     T data;
     // forward pointer
     dnode<T> *next;
     // backward pointer
     dnode<T> *last;
-
-public:
     // TODO:add member functions
     dnode(T d, dnode<T> *n, dnode<T> *l);
-}
+    dnode(T d);
+    void insert(dnode<T> node, T val);
+};
+
+bool operator<(const dnode<int>& n1, const dnode<int>& n2);
