106568b5bfd8a9c51e0f477547b1c0259e4b5eba
Anshuman-UCSB <Anshuman@ucsb.edu>
2020-03-03 00:25:36 -0800

    moved dnode in

--- dnode.h
+++ dnode.h
@@ -23,6 +23,67 @@ public:
 template<class T>
 bool operator<(dnode<T> t1, dnode<T> t2);
 
-#include "dnode.cpp"
+
+
+using namespace std;
+
+template <class T>
+dnode<T>::dnode(T d, dnode<T> *n, dnode<T> *l){
+    next = n;
+    last = l;
+    data = d;
+}
+
+template <class T>
+dnode<T>::dnode(T d)
+{
+    next = 0;
+    last = 0;
+    data = d;
+}
+
+template <class T>
+bool operator<(dnode<T> t1, dnode<T> t2){
+    return t1.data<t2.data;
+}
+
+template<class T>
+void dnode<T>::insert(dnode<T> *&head, dnode<T> *node)
+{
+    if (*node < *head)
+    {
+        head->last = node;
+        node->next = head;
+        head = head->last;
+        return;
+    }
+    //if node is at beginning
+
+    dnode<T> *end = head;
+    while (end->next != 0)
+    {
+        end = end->next;
+    }
+    if (!(*node < *end))
+    {
+        end->next = node;
+        node->last = end;
+        return;
+    }
+    //if node is at end
+
+    dnode<T> *p = head;
+    while (!(*node < *p))
+    {
+        p = p->next;
+    }
+    node->last = p->last;
+    node->next = p;
+    p->last->next = node;
+    p->last = node;
+
+    //if node is in middle
+}
+
 
 #endif
--- wordsearchunion.cpp
+++ wordsearchunion.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <fstream>
 #include<string>
+#include "bag.h"
 
 // ADD ANY HEADER FILES YOU NEED
 
